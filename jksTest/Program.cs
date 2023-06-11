654c0f3a455bb7a6312579f2a0868c045b89e333
cwl625 <cwlee@dowoomets.co.kr>
2023-06-12 02:26:45 +0900

    asdasdaa

--- jksTest/Program.cs
+++ jksTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,9 @@ namespace jksTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+
+            //
+            Debug.WriteLine("Hello");
 
         }
     }
